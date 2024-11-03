7fdf41707cc6686fbdfea9957dd7782f94170302
Augene Pak <augene.pak@outlook.com>
2024-11-03 14:26:15 -0800

    v2.0.0

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 # Synonym or Antonym?
 
-This is a game that expands one's knowledge of vocabulary through having one guess whether a word is a synonym or an antonym of another.
+**Synonym or Antonym?** is a simple terminal-based game, where the user guesses whether two words are synonyms or antonyms to each other.
 
 ## Instructions
 
 You need Python to play this game.
 ```console
-cd 'FILE LOCATION WHERE THE GAME WAS DOWNLOADED'
+cd 'FOLDER WHERE GAME WAS INSTALLED'
 python game.py
 ```
-![Example](example.png)
-
-## Changelog
-
-V1 - Initially built as a text-based game with Python.
+![Example](img/example.png)
 
 ## Resources Utilized
 
-This game was made possible by the [Free Dictionary API](https://dictionaryapi.dev/).
+The initial data of words and their synonyms and antonyms was provided by the [data/english_synonyms_and_antonyms.csv](https://github.com/SuzanaK/english_synonyms_antonyms_list/blob/master/data/english_synonyms_and_antonyms.csv) file from the [SuzanaK/english_synonyms_antonyms_list](https://github.com/SuzanaK/english_synonyms_antonyms_list/) repository on GitHub.
 
-The initial list of words was provided by the [API itself](https://github.com/meetDeveloper/freeDictionaryAPI/blob/master/meta/wordList/english.txt) and [dwyl's english-words repository](https://github.com/dwyl/english-words/blob/master/words_alpha.txt), which were processed to reduce loading time using the [generate_list.py file](https://github.com/augene/synonym-or-antonym/blob/main/generate_list.py).
+## Changelog
+
+v1.0.0 (8/31/2021)
+- Initial version
+- Utilized the [Free Dictionary API](https://dictionaryapi.dev/) and [dwyl/english-words](https://github.com/dwyl/english-words/blob/master/words_alpha.txt)
+
+v2.0.0 (11/3/2024)
+- Added requirements.txt
+- Added ASCII art at the start of each play
+- Changed data source to [SuzanaK/english_synonyms_antonyms_list](https://github.com/SuzanaK/english_synonyms_antonyms_list/)
 
 ## License
 
-Synonym or Antonym? is licensed under the [GNU GPLv3](https://github.com/augene/synonym-or-antonym/blob/main/LICENSE).
-
-## Future Development Plans
-
-I hope to create a website version and an Android app for the game in the future as I learn more about programming.
+**Synonym or Antonym?** is licensed under [GNU GPLv3](https://github.com/augene/synonym-or-antonym/blob/main/LICENSE).
--- game.py
+++ game.py
@@ -1,103 +1,120 @@
+"""Terminal-based game, where the user guesses whether two words are synonyms or antonyms."""
+
 # Augene Pak
 # Synonym or Antonym?
 
-import json
-import requests
 import random
+import pandas as pd
 
-possible_words = list(open('words.txt').read().split())
-word_list = ['']
-data = ['']
-score = 0
-lost = False
-play_again = "y"
-syn_num = 0
-ant_num = 0
-random_number = 0
-word_list = []
+LINE_LENGTH = 145
 
-while play_again.startswith("y"):
-    while not lost:
-        # detect whether word exists in thesaurus
-        while syn_num == 0 or ant_num == 0:
-            i = 0
-            syn_num = 0
-            ant_num = 0
+def generate_synonyms_antonyms_dict():
+    """Return an in-memory dictionary of synonyms & antonyms from external file."""
+    synonyms_antonyms_df = pd.read_csv('data/english_synonyms_and_antonyms.csv', sep='\t',
+                                     names=['word', 'synonyms', 'antonyms']).fillna('')
 
-            # get random word that exists in API
-            word = possible_words[random.randint(0, len(possible_words) - 1)]
-            random_num = random.randint(0, 1)
+    synonyms_antonyms_dict = {}
+    for row in range(len(synonyms_antonyms_df)):
+        word = synonyms_antonyms_df.iloc[row]['word']
+        synonyms = synonyms_antonyms_df.iloc[row]['synonyms'].split(', ')
+        antonyms = synonyms_antonyms_df.iloc[row]['antonyms'].split(', ')
 
-            # request data using word
-            rqst_st = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
-            response = requests.get(rqst_st)
-            data = json.loads(response.text)
+        if synonyms == ['']:
+            synonyms = []
 
-            if str(type(data)) == "<class 'dict'>":
-                continue
+        if antonyms == ['']:
+            antonyms = []
 
-            for i in range(len(data[0]["meanings"])):
-                syn_num += \
-                    len(data[0]["meanings"][i]["definitions"][0]["synonyms"])
-                ant_num += \
-                    len(data[0]["meanings"][i]["definitions"][0]["antonyms"])
+        synonyms_antonyms_dict[word] = {'synonyms': synonyms, 'antonyms': antonyms}
+    return synonyms_antonyms_dict
 
-            # if (random_num == 0 and syn_num == 0) or \
-            #         (random_num == 1 and ant_num == 0):
-            #     syn_num = 0
-            #     ant_num = 0
-            #     continue
+def print_synonym_or_antonym_ascii():
+    """Prints the introductory ASCII art."""
+    # Generated using https://patorjk.com/software/taag/#p=display&h=0&f=Standard&t=Synonym%20or%20Antonym%3F
+    print(" ____                                                                              _              _                                         ___ ")      
+    print("/ ___|   _   _   _ __     ___    _ __    _   _   _ __ ___       ___    _ __       / \\     _ __   | |_    ___    _ __    _   _   _ __ ___   |__ \\")
+    print("\\___ \\  | | | | | '_ \\   / _ \\  | '_ \\  | | | | | '_ ` _ \\     / _ \\  | '__|     / _ \\   | '_ \\  | __|  / _ \\  | '_ \\  | | | | | '_ ` _ \\    / /")
+    print(" ___) | | |_| | | | | | | (_) | | | | | | |_| | | | | | | |   | (_) | | |       / ___ \\  | | | | | |_  | (_) | | | | | | |_| | | | | | | |  |_| ")
+    print("|____/   \\__, | |_| |_|  \\___/  |_| |_|  \\__, | |_| |_| |_|    \\___/  |_|      /_/   \\_\\ |_| |_|  \\__|  \\___/  |_| |_|  \\__, | |_| |_| |_|  (_) ")
+    print("         |___/                           |___/                                                                          |___/                   ")
+    print(" " * LINE_LENGTH)
+    print("=" * LINE_LENGTH)
 
-        # stores appropriate word list
-        # if synonym list doesn't exist, switch to antonym list; vice versa
-        if random_num == 0:
-            for i in range(len(data[0]["meanings"])):
-                word_list += \
-                    data[0]["meanings"][i]["definitions"][0]["synonyms"]
+
+def select_word_pair(synonyms_antonyms, possible_words):
+    """Randomly select and return tuple of words that are synonyms or antonyms to each other."""
+    word_1 = possible_words[random.randint(0, len(possible_words) - 1)]
+
+    synonyms = synonyms_antonyms[word_1]['synonyms']
+    antonyms = synonyms_antonyms[word_1]['antonyms']
+
+    # if there are no synonyms, choose from antonyms; vice versa
+    # syn_or_ant -> synonym = 0, antonym = 1
+    if not synonyms:
+        syn_or_ant = 1
+        word_2 = antonyms[random.randint(0, len(antonyms) - 1)]
+    elif not antonyms:
+        syn_or_ant = 0
+        word_2 = synonyms[random.randint(0, len(synonyms) - 1)]
+
+    else:
+        # 0 = synonym, 1 = antonym
+        syn_or_ant = random.randint(0, 2)
+        if syn_or_ant == 0:
+            synonyms = synonyms_antonyms[word_1]['synonyms']
+            word_2 = synonyms[random.randint(0, len(synonyms) - 1)]
         else:
-            for i in range(len(data[0]["meanings"])):
-                word_list += \
-                    data[0]["meanings"][i]["definitions"][0]["antonyms"]
+            antonyms = synonyms_antonyms[word_1]['antonyms']
+            word_2 = antonyms[random.randint(0, len(antonyms) - 1)]
 
-        # remove duplicates
-        word_list = set(word_list)
-        word_list = list(word_list)
+    return (word_1, word_2, syn_or_ant)
 
-        # select random word to pair with first word
-        word2 = word_list[random.randint(0, len(word_list) - 1)]
+def play_game():
+    """Runs the main game logic."""
+    print_synonym_or_antonym_ascii()
 
-        print(word + " & " + word2)
-        response = input("Synonym or Antonym? (s/a) ")
+    synonyms_antonyms = generate_synonyms_antonyms_dict()
+    possible_words = list(synonyms_antonyms.keys())
 
-        # only allow responses starting with s or a
-        while not (response.startswith("s") or response.startswith("a")):
-            print("Try again")
-            response = input("Synonym or Antonym? (s/a) ")
-
-        if (response.lower().startswith("s") and random_num == 0) or \
-                (response.lower().startswith("a") and random_num == 1):
-            print("Success!\n")
-            score += 1
-            data = ['']
-        else:
-            print("Failure :(\n")
-            lost = True
-        syn_num = 0
-        ant_num = 0
-        word_list = []
-
-    # display if game ends
-    print("Your score is " + str(score) + ".")
-
-    play_again = input("Play again? (y/n) ")
-    while not (play_again.startswith("y") or play_again.startswith("n")):
-        print("Try again")
-        play_again = input("Play again? (y/n) ")
-
-    if play_again.startswith("y"):
-        lost = False
-        data = ['']
+    play_again = 'y'
+    lost = False
+    while play_again.startswith('y'):
         score = 0
-        print("-----------------------------------------------------")
-    elif play_again.startswith("n"):
-        print("\nThanks for playing!")
+        while not lost:
+            print('Score: ' + str(score))
+            word_1, word_2, syn_or_ant = select_word_pair(synonyms_antonyms, possible_words)
+
+            print(word_1 + ' & ' + word_2)
+            response = input('Synonym or Antonym? (s/a) ').lower()
+
+            # only allow responses starting with s or a
+            while not (response.lower().startswith('s') or response.lower().startswith('a')):
+                response = input('Please enter either s or a! (s/a) ').lower()
+
+            if (response.startswith('s') and syn_or_ant == 0) or \
+                    (response.startswith('a') and syn_or_ant == 1):
+                print('Correct :)')
+                print('-' * LINE_LENGTH)
+                score += 1
+            else:
+                print('Incorrect :(')
+                print('-' * LINE_LENGTH)
+                lost = True
+
+        # display if game ends
+        print('Your final score is ' + str(score) + '.')
+        print('-' * LINE_LENGTH)
+
+        play_again = input('Play again? (y/n) ').lower()
+        while not (play_again.startswith('y') or play_again.startswith('n')):
+            play_again = input('Please enter either y or n! (y/n) ').lower()
+
+        print('=' * LINE_LENGTH)
+        if play_again.startswith('y'):
+            lost = False
+            score = 0
+        elif play_again.startswith('n'):
+            print('Thanks for playing! :)')
+
+
+play_game()
